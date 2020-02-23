843a7c5d25c9ceae847f25fff23cce41d04411bd
Eri-Gutierrez <37821946+Eri-Gutierrez@users.noreply.github.com>
2020-02-23 12:38:52 -0800

    Create mux.v

---
+++ mux.v
@@ -0,0 +1,15 @@
+// Course: CSE 401- Computer Architecture
+// Term: Winter 2020
+// Name: Erika Gutierrez
+// ID: 005318270
+
+`timescale 1ns / 1ps
+
+module mux(
+    input wire [31:0] s0, s1,
+    input wire select,
+    output wire [31:0] npc
+    );
+
+assign npc = select ? s0 : s1;
+endmodule // mux
